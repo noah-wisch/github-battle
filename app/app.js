01462a5a00c9c88a79c84e464cc444e41d4738c3
Noah Wisch <noah.wisch@gmail.com>
2017-05-31 10:04:46 -0400

    webpack compiling error resolved

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// require('./index.css');
 
 // components are concerned w/ 3 things: state, lifecycle event, UI
 
 class App extends Component {
   render() { // used to describe lifecycle of component
-    return(
+    return (
       <div>Hello World!</div>
     );
   }
@@ -16,17 +17,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
-
-// var something = "something"
-// if (true) {
-//   var something = "something else"
-// }
-//
-// console.log(something);
-//
-// let something = "something"
-// if (true) {
-//   something = "something else"
-// }
-//
-// console.log(something)
