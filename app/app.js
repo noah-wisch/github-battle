8e7fc47e65e2c6b6395233014db2a49c4d89415e
Noah Wisch <noah.wisch@gmail.com>
2017-06-02 14:23:11 -0400

    example information

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 // var ReactDOM = require('react-dom');
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 require('./style.css');
 
 // components are concerned w/ 3 things: state, lifecycle event, UI
@@ -9,10 +10,25 @@ require('./style.css');
 class App extends Component {
   render() { // used to describe lifecycle of component
     return (
+      // <div>
+      //   <img
+      //     src={this.props.img}
+      //     alt='Avatar'
+      //     style={{width: 100, height: 100}}
+      //   />
+      //   <h1>Name: {this.props.name}</h1>
+      //   <h3>Username: {this.props.username}</h3>
+      // </div>
       <div>Hello World!</div>
     );
   }
 }
+
+// Badge.propTypes = {
+//   img: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   username: PropTypes.string.isRequired,
+// }
 
 ReactDOM.render(
   <App />,
